03c941f20ba4231a18687f761bd858da7c659f53
Ryam Lael <128926385+RyamLael@users.noreply.github.com>
2023-12-14 23:37:41 -0300

    Update Exercicio8.java

--- Lista02/Exercicio8.java
+++ Lista02/Exercicio8.java
@@ -7,14 +7,13 @@ public class Exercicio8 {
 		Scanner leitor = new Scanner(System.in);
 		String expression;
 		int a, b;
-		//char a;
 		
 		System.out.println("Digite uma expressão matemática: ");
-		expression= leitor.nextLine();
+		expression = leitor.nextLine();
 		
-		for(a=0;a<expression.length();a++) {
-			if(expression.charAt(a)=='(') {
-				for(b=0;b<expression.length();b++) {
+		for(a=0; a<expression.length(); a++) {
+			if(expression.charAt(a) == '(') {
+				for(b=0; b<expression.length(); b++) {
 					if(!(expression.charAt(a)==')')) {
 						System.out.println("Está faltando um )");
 					}
@@ -25,10 +24,6 @@ public class Exercicio8 {
 			System.out.print(expression.charAt(a)+ " ");
 		}
 		
-		
-		
-		
-
 	}
 
 }
